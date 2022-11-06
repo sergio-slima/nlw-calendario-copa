2dae81d5bb0682341ccb8c2376b02fc66e9e830f
sergio-slima <sergio_silvalima@hotmail.com>
2022-11-06 10:16:02 -0300

    personalizando o projeto

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ function createGame(player1, hour, player2) {
   `
 }
 
+let delay = 0;
 function createCard(date, day, games) {
+  delay = delay + 0.3
   return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s">
       <h2>${date} <span>${day}</span></h2>
       <ul>
         ${games}
@@ -19,20 +21,11 @@ function createCard(date, day, games) {
   `
 }
 
-document.querySelector("#app").innerHTML = `
-  <header>
-    <img src="./assets/logo.svg" alt="Logo do Calendário da Copa" />
-  </header>
-  <main id="cards">
-    ${createCard("24/11", "quinta", 
-      createGame("brazil", "16:00", "serbia"))
-    }
-    ${createCard("28/11", "terça", 
+document.querySelector("#cards").innerHTML = 
+  createCard("24/11", "quinta", 
+      createGame("brazil", "16:00", "serbia")) +
+  createCard("28/11", "terça", 
       createGame("brazil", "13:00", "serbia") + 
-      createGame("portugal", "16:00", "uruguay"))
-    }
-    ${createCard("04/12", "domingo", 
+      createGame("portugal", "16:00", "uruguay")) +
+  createCard("04/12", "domingo", 
       createGame("brazil", "16:00", "serbia"))
-    }
-  </main>
-`
